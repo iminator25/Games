a4eb10b787f9f8e61a1e343c606da0f8df395ebb
Imran Adamjee <imey2597@gmail.com>
2020-11-15 23:00:15 -0600

    Initial commit

---
+++ Game_test.go
@@ -0,0 +1,20 @@
+package Games
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGETPlayers(t *testing.T) {
+	request, _ := http.NewRequest(http.MethodGet,"/players/Pepper",nil)
+	response := httptest.NewRecorder()
+	PlayerServer(response, request)
+
+	got := response.Body.String()
+	want := "20"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got ,want)
+	}
+}
